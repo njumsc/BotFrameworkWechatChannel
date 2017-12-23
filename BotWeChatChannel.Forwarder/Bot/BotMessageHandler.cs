01a81b55992e4897c1b20800ced150534ffaa383
Chen Junda <smallda@outlook.com>
2017-12-23 18:10:01 +0800

    fixed exception

--- BotWeChatChannel.Forwarder/Bot/BotMessageHandler.cs
+++ BotWeChatChannel.Forwarder/Bot/BotMessageHandler.cs
@@ -33,7 +33,6 @@ namespace BotWeChatChannel.Forwarder.Bot
 
         public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
         {
-            requestMessage.
             throw new NotSupportedRequestTypeException();
         }
 
